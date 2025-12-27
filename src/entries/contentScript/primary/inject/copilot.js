9cc49d33f34dd5732780d3f11def35587fe93ae4
Yurei TZK <me@yureitzk.com>
2025-12-27 12:07:07 +0300

    feat: hide copilot search answer elemenent

--- src/entries/contentScript/primary/inject/copilot.js
+++ src/entries/contentScript/primary/inject/copilot.js
@@ -5,10 +5,25 @@ export function main() {
 		[data-ext-hide-bing-copilot] #b_copilot_search_container,
 		[data-ext-hide-bing-copilot] #b-scopeListItem-copilotsearch,
 		[data-ext-hide-bing-copilot] #b-scopeListItem-conv,
-		[data-ext-hide-bing-copilot] .b_phead_chat_link  {
+		[data-ext-hide-bing-copilot] .b_phead_chat_link,
+		[data-ext-hide-bing-copilot-el] {
 			display: none !important; 
 		}
 `;
 	document.documentElement.appendChild(style);
 	document.documentElement.setAttribute('data-ext-hide-bing-copilot', '1');
+
+	const observer = new MutationObserver(() => {
+		const copilotAnswerContainerEl = document.querySelector('#copans_container');
+		if (copilotAnswerContainerEl) {
+			let parentEL = copilotAnswerContainerEl.closest('li');
+
+			if (parentEL) {
+				parentEL.setAttribute('data-ext-hide-bing-copilot-el', 'true');
+				observer.disconnect();
+			}
+		}
+	});
+
+	observer.observe(document, { childList: true, subtree: true });
 }
